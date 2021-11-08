d0b9d83bc1e8027514e4ce088009a5c0fc65ec8d
george <george@gomaji.com>
2021-11-08 23:42:16 +0800

    [AS] first commit

--- code/index.php
+++ code/index.php
@@ -1,2 +1,27 @@
 <?php
-phpinfo();
+// phpinfo();
+// echo $_SERVER['REQUEST_URI'];exit;
+
+// (A) OPEN IMAGE
+$img = imagecreatefromjpeg("sample.jpeg");
+
+// (B) WRITE TEXT
+$txt = "Hello World";
+// $fontFile = "C:\Windows\Fonts\arial.ttf"; // CHANGE TO YOUR OWN!
+$font = "Arial.ttf";
+$fontSize = 32;
+$fontColor = imagecolorallocate($img, 128, 128, 128);
+$posX = 5;
+$posY = 44;
+$angle = 0;
+imagettftext($img, $fontSize, $angle, $posX, $posY, $fontColor, $font, $txt);
+
+// (C) OUTPUT IMAGE
+// (C1) DIRECTLY SHOW IMAGE
+header("Content-type: image/jpeg");
+imagejpeg($img);
+imagedestroy($img);
+
+// (C2) OR SAVE TO A FILE
+// $quality = 100; // 0 to 100
+// imagejpeg($img, "demo.jpg", $quality);
